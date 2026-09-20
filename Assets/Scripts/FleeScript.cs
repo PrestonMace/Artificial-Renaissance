1333faeac4643ff8529a8b225fea40daebf80c9f
Westin Maymir <WMaymir@my.gcu.edu>
2026-09-19 18:48:13 -0700

    bc ndfgbdfvgbedfvgbed

--- Assets/Scripts/FleeScript.cs
+++ Assets/Scripts/FleeScript.cs
@@ -14,6 +14,11 @@ public class Flee : MonoBehaviour
 
     private Vector3 _direction;
 
+    //for whisker code
+    private Vector3 whiskerOrigin;
+    private float whiskerMaxDistance = 2f;
+    private float wReturnForceStregnth = 4f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,9 +32,59 @@ public class Flee : MonoBehaviour
         _direction = transform.position - EvadeTF.position;
     }
 
+    private void AvoidObstacles()
+    {
+
+        whiskerOrigin = AITF.position;
+
+        //make 5 of them rotated on the Z axis in even increments
+        Vector3[] whiskerDirections = new Vector3[]
+        {
+        _direction.normalized,
+        Quaternion.AngleAxis(45, Vector3.up) * _direction.normalized,
+        Quaternion.AngleAxis(-45, Vector3.up) * _direction.normalized,
+        Quaternion.AngleAxis(90, Vector3.up) * _direction.normalized,
+        Quaternion.AngleAxis(-90, Vector3.up) * _direction.normalized,
+        };
+
+        //impliments functionality for each whisker
+        foreach (Vector3 dir in whiskerDirections)
+        {
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(whiskerOrigin, dir, out hitInfo, whiskerMaxDistance) && hitInfo.collider.gameObject.tag != "Wall")
+            {
+                //Debug.Log("Hit " + hitInfo.collider.name);
+
+                //get the distance between the end of the whisker and the point it collided at
+                Vector3 pointOfCollision = hitInfo.point;
+                Vector3 whiskerEndPos = whiskerOrigin + dir * whiskerMaxDistance;
+                float whiskerPenetrationDistance = (whiskerEndPos - pointOfCollision).magnitude;
+
+                //allows us to tweak the distance-based force
+                float whiskerReturnForce = whiskerPenetrationDistance * wReturnForceStregnth;
+
+                //gets the repel direction from the center of the object so there is no equilibrium
+                Vector3 repelDirection = (transform.position - hitInfo.collider.gameObject.transform.position);
+                float repelForce = repelDirection.magnitude * math.abs(whiskerReturnForce);
+
+                //applies the final force
+                AIRB.AddForce(repelDirection.normalized * repelForce);
+
+                //for viewport debugging
+                Debug.DrawRay(hitInfo.point, hitInfo.normal, Color.green);
+            }
+            else
+            {
+                Debug.DrawRay(whiskerOrigin, dir * whiskerMaxDistance, Color.red);
+            }
+        }
+    }
+
     private void FixedUpdate()
     {
         AIRB.AddForce(_direction.normalized * _maxAcceleration, ForceMode.Impulse);
+        AvoidObstacles();
     }
 
 }
